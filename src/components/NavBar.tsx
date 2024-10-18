22428adf0e4fdb6c6fad40b35e61aa8c616d6b7a
nikola <nikodola@gmail.com>
2024-10-18 13:50:37 +0200

    nav bar fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useUser } from "@/contexts/userContext";
 import Link from "next/link";
 import Notification from "./Notification";
@@ -7,19 +7,22 @@ import Notification from "./Notification";
 export default function NavBar(){
     const {logoutUser, user} = useUser()
     const router = useRouter()
-  
+    const path = usePathname()
+   
     return(
         <nav className="navbar z-10">
             <ul className="flex justify-end gap-4 [&]:mx-14 relative">
                 {user ? 
                 <>
-                    <li><Link href={'/admin'}> Admin</Link></li>
-                    <li><Link href={'/admin/departments'}> Departments</Link></li>
-                    <li className="cursor-pointer" onClick={logoutUser}>Logout</li>
+                    <li className={ path === '/' ? 'navLinkActive': 'navLinkInactive'}><Link href={'/'}> Counter</Link></li>
+                    <li className={ path === '/admin' ? 'navLinkActive': 'navLinkInactive'}><Link href={'/admin'}> Admin</Link></li>
+                    <li className={ path === '/admin/departments' ? 'navLinkActive': 'navLinkInactive'}><Link href={'/admin/departments'}> Departments</Link></li>
+                    <li  className="cursor-pointer" onClick={logoutUser}>Logout</li>
+          
                 </>:
                 <>
-                    <li><Link href={'/'}> Counter</Link></li>
-                    <li className="cursor-pointer"><Link href={'/login'}>login</Link></li>
+                    <li className={ path === '/' ? 'navLinkActive': 'navLinkInactive'}><Link href={'/'}> Counter</Link></li>
+                    <li className={ path === '/login' ? 'navLinkActive': 'navLinkInactive'}><Link href={'/login'}>login</Link></li>
                 </>
                 }
                 <Notification/>

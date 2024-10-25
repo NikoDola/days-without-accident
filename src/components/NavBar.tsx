386160932837c998e612d2de8071f0d168a459fb
nikola <nikodola@gmail.com>
2024-10-25 03:04:42 +0200

    nav menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,18 @@ import { usePathname } from "next/navigation";
 import { useUser } from "@/contexts/userContext";
 import Link from "next/link";
 import Notification from "./Notification";
-import { useState } from "react";
+import "@/components/css-components/NavBar.css"
+import { useState, useEffect } from "react";
+
 
 export default function NavBar(){
     const {logoutUser, user} = useUser();
-    const [burger, setBurger] = useState(false)
+    const [burger, setBurger] = useState(true)
+
+    useEffect(()=>{
+     const checkingInnerWith = innerWidth
+     checkingInnerWith < 1000 && setBurger(false)
+    },[])
 
     const path = usePathname();
 
@@ -15,43 +22,39 @@ export default function NavBar(){
     const isDepartmentsPath = path.startsWith('/admin/departments');
 
     const handleBurger = () =>{
-        !burger ? setBurger(true): setBurger(false)
+      burger ? setBurger(false): setBurger(true)
     }
     return (
         <nav className="navbar">
           {user ? (
-            <div className="loginNav">
-                
-              <ul className={burger? "navUls" :"navUrls"}>
-                <li className={path === '/' ? '' : ''}>
-                  <Link href="/">Counter</Link>
-                </li>
-      
-                <li className={isDepartmentsPath ? 'navLinkActive' : 'navLinkInactive'}>
-                  <Link href="/admin/departments">Departments</Link>
-                </li>
-      
-                <li className="navLinkInactive cursor-pointer" onClick={logoutUser}>
-                  Logout
-                </li>
+            <div className="loggedIn">
+              <ul className={burger ? "navUrls": "navUrlsHidden"}>
+                <li className="navLinks"><Link href={"/"}>Counter</Link></li>
+                <hr className="hrDecoration"/>
+                <li className="navLinks"><Link href="/admin/departments">Departments</Link></li>
+                <hr className="hrDecoration"/>
+                <li className="navLinks">Accidents</li>
+                <hr className="hrDecoration"/>
+                <li className="navLinks">Employees</li>
               </ul>
-              <div className="mobileMenu">
+              <div className="notificationAndBurger">
                 <Notification />
-                <div onClick={handleBurger} className="burger"></div>
+                <li className="navLinkInactive cursor-pointer" onClick={logoutUser}>Logout</li>
+                <div onClick={handleBurger} className="burger">
+                  <div className="top"></div>
+                  <div className="middle"></div>
+                  <div className="bottom"></div>
+                </div>
               </div>
           
             </div>
           ) : (
-            <>
+            <div className="loggedOut">
               <ul className="navUls">
-                <li className={path === '/' ? 'navLinkActive' : 'navLinkInactive'}>
-                  <Link href="/">Counter</Link>
-                </li>
-                <li className={path === '/login' ? 'navLinkActive' : 'navLinkInactive'}>
-                  <Link href="/login">Login</Link>
-                </li>
+                <li ><Link href="/">Counter</Link></li>
+                <li><Link href="/login">Login</Link></li>
               </ul>
-            </>
+            </div>
           )}
         </nav>
       );
